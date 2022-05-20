91c721fae8f750e107ef3013c7f5023b1950b1e6
Aston Cook <coo19025@byui.edu>
2022-05-20 10:59:40 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 8080;
+const connect = require('./db/connect');
+
+connect.initDb();
+
+app
+.use(bodyParser.json())
+.use('/', require('./routes'));
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+});
